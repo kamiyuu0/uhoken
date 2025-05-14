05f780731af3d6824688c9d99b14324ce1c18e2a
yuekami <yuekami7@gmail.com>
2025-05-14 16:30:20 +0900

    feat: 正解数をカウント

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,8 +1,27 @@
 class QuestionController < ApplicationController
   require 'yaml'
+
   def show
     @id = params[:id].to_i
     @questions = reaction_hash.dig('questions')[@id - 1]
+    if @id == 1
+      session[:result] = 0
+    end
+  end
+
+  def update
+    @id = params[:id]
+    answer = reaction_hash.dig('questions')[@id.to_i - 1].dig('a')
+    choice = params[:choice].to_i
+    if answer == choice
+      session[:result] += 1
+    end
+
+    if @id == '5'
+      redirect_to result_path
+    else
+      redirect_to question_path(@id.to_i + 1)
+    end
   end
 
   private
@@ -10,4 +29,5 @@ class QuestionController < ApplicationController
   def reaction_hash
     YAML.load_file("questions.yml")
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :question, only: %i[show]
+  resources :question, only: %i[show update]
   root "static_pages#top"
   get "result", to: "static_pages#result"
 end
